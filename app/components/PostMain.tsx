889664d1037f0bc53889c0be5f10a7408a885692
unknown <emmco96@gmail.com>
2024-06-07 08:00:40 -0700

    new

--- app/components/PostMain.tsx
+++ app/components/PostMain.tsx
@@ -28,29 +28,43 @@ export default function PostMain({ post }: PostMainCompTypes) {
         <>
             <div id={`PostMain-${post.id}`} className="border-b border-b-dark-4 py-6">
 
-                <div className="cursor-pointer">
+<div id="outer">
+<div id="inner">
+         <div className="cursor-pointer">
                     <img className="rounded-full max-h-[60px]" width="60" src={useCreateBucketUrl(post?.profile?.image)} />
                 </div>
-
-                <div className="pl-3 w-full px-4">
-                    <div className="flex items-center justify-between pb-0.5">
+                   <div >
                         <Link href={`/profile/${post.profile.user_id}`}>
                             <span className="font-bold text-light-orange hover:underline cursor-pointer">
                                 {post.profile.name}
                             </span>
                         </Link>
+                        <p className="text-[15px] text-gray-400 pb-0.5 break-words md:max-w-[400px] max-w-[300px]">{post.text}</p>
+                    </div>
+</div>
 
-                        <button className="border border-light-orange text-[15px] px-[21px] py-0.5 text-light-orange hover:bg-red-1 font-semibold rounded-md">
+
+<div>
+    <button id="button" className="border border-light-orange text-[15px] px-[21px] py-0.5 text-light-orange hover:bg-red-1 font-semibold rounded-md">
                             Follow
                         </button>
-                    </div>
-                    <p className="text-[15px] text-gray-400 pb-0.5 break-words md:max-w-[400px] max-w-[300px]">{post.text}</p>
+</div>
+</div>
+
+
+                    
+
+                <div className="pl-3 w-full px-4">
+                    <div>
+                     
                     <p className="text-[14px] text-gray-400 pb-0.5">#fun #cool #SuperAwesome</p>
                     <p className="text-[14px] text-gray-400 pb-0.5 flex items-center font-semibold">
                         <ImMusic size="17"/>
                         <span className="px-1 text-gray-400">original sound - AWESOME</span>
                         <AiFillHeart size="20"/>
-                    </p>
+                    </p>   
+                    </div>
+                    
 
                     <div className="mt-2.5 flex">
                         <div
