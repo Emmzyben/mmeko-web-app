e2a2cb1250f7d2f4742dd2eb93d8a5f3a8ea1bfb
unknown <emmco96@gmail.com>
2024-06-08 01:21:36 -0700

    new

---
+++ app/wallet/page.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { categoryList } from "@/constants/categories";
+import { useEffect, useState } from "react";
+import useCreateHost from "../hooks/useCreateHost";
+import { useUser } from "../context/user";
+import { useRouter } from "next/navigation";
+import { UploadError } from "../types";
+import ImageUpload from "../components/ImageUpload.";
+import "../components/style.css"
+
+export default function wallet() {
+ 
+  
+  return (
+    <div>
+        
+    </div>
+  );
+}
+
