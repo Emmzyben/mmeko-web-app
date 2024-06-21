9cbcc9249bc6cc73970323bc1963cf608031386a
unknown <emmco96@gmail.com>
2024-06-21 17:25:36 +0100

    new

--- app/hooks/useUpdateProfile.tsx
+++ app/hooks/useUpdateProfile.tsx
@@ -2,13 +2,13 @@ import { database } from "@/libs/AppWriteClient";
 const DATABASE_ID = '6647001200115f436fbf';
 const COLLECTION_ID_PROFILE = '66470137001632d54f1d';
 
-const useUpdateProfile = async (id: string, data: { [key: string]: any }) => {
+const useUpdateProfile = async (id: string, ...data: { [key: string]: any }[]) => {
   try {
     await database.updateDocument(
       DATABASE_ID,
       COLLECTION_ID_PROFILE,
       id,
-      data
+      ...data
     );
   } catch (error) {
     console.error("Failed to update profile:", error);
