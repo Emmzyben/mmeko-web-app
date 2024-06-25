41af1b1afba2b959f3e8086dd4c44fe449a39e23
unknown <emmco96@gmail.com>
2024-06-25 14:22:28 +0100

    new

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -69,7 +69,7 @@ export default function Profile({ params }: ProfilePageTypes) {
     return (
         <>
             <MainLayout>
-                <div className="pt-[90px] ml-[90px] 2xl:pl-[185px] lg:pl-[160px] lg:pr-0 w-[calc(100%-90px)] pr-3 max-w-[1800px] 2xl:mx-auto">
+                <div>
 
                     <div className="flex">
                         <ClientOnly>
